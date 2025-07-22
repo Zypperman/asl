3ed1037ddf4cc2d1f8318e32880d3439a57c9e74
Larry <larry.lee.jia.heng@gmail.com>
2025-07-22 14:02:31 +0800

    fix(requirements): specified package versions with minor script formattting fixes

--- application.py
+++ application.py
@@ -2,31 +2,35 @@ from flask import Flask, render_template, Response
 import ultralytics
 from resources.detect import detect
 
+
 def create_app(config_filename):
-    application = app = Flask(__name__, static_url_path='')
+    application = app = Flask(__name__, static_url_path="")
     application.config.from_object(config_filename)
 
-    PATH = 'model/best.pt'
+    PATH = "model/best.pt"
     model = ultralytics.YOLO(PATH)
 
     return [application, model]
 
+
 app, model = create_app("config")
 application = app
 
-@application.route('/health', methods=['GET'])
 
+@application.route("/health", methods=["GET"])
 def health():
-    return 'ASL App Running!'
+    return "ASL App Running!"
 
-@application.route('/', methods=['GET'])
 
+@application.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict')
+
+@app.route("/predict")
 def predict():
-    return Response(detect(model), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(detect(model), mimetype="multipart/x-mixed-replace; boundary=frame")
+
 
 if __name__ == "__main__":
     application.run()
--- resources/detect.py
+++ resources/detect.py
@@ -1,11 +1,13 @@
 import cv2
+
 # import cvzone
 # from cvzone.SelfiSegmentationModule import SelfiSegmentation
 # import os
 # import time
 
+
 def detect(model):
-    words = ['SMUBIA', 'SCIS']
+    words = ["SMUBIA", "SCIS"]
     current_word = 0
     current_letter = 0
     # pTime = 0
@@ -13,13 +15,14 @@ def detect(model):
 
     while True:
         cap = cv2.VideoCapture(0)
-        #cap.set(cv2.CAP_PROP_FPS, 60)
-        #segmentor = SelfiSegmentation()
+        # cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+        # cap.set(cv2.CAP_PROP_FPS, 60)
+        # segmentor = SelfiSegmentation()
 
         if not (cap.isOpened()):
             print("Could not open video device")
             break
-            
+
         else:
 
             _, frame = cap.read()
@@ -28,34 +31,86 @@ def detect(model):
             # cTime = time.time()
             # fps = 1 / (cTime - pTime)
             # pTime = cTime
-            # cv2.putText(frame, f'FPS: {int(fps)}', (frame.shape[1] - 150, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            # cv2.putText(
+            #    frame,
+            #    f"FPS: {int(fps)}",
+            #    (frame.shape[1] - 150, 50),
+            #    cv2.FONT_HERSHEY_SIMPLEX,
+            #    0.9,
+            #    (36, 255, 12),
+            #    2,
+            # )
 
             result = model(frame)
             prediction = result[0]
-            
+
             boxes = prediction.boxes
             characters = prediction.names
-            
+
             if len(boxes) > 0:
                 box = boxes[0]
                 label = characters[int(box.cls[0].numpy())]
-                if current_word != len(words) and label == words[current_word][current_letter]:
+                if (
+                    current_word != len(words)
+                    and label == words[current_word][current_letter]
+                ):
                     current_letter += 1
                     if current_letter == len(words[current_word]):
                         current_word += 1
                         current_letter = 0
 
-                x,y,w,h = box.xyxy[0].numpy()
-                frame = cv2.rectangle(frame, (int(x), int(y)), (int(w), int(h)), (36,255,12), 1)
-                cv2.putText(frame, f'Label: {label}', (int(x), int(y)-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                x, y, w, h = box.xyxy[0].numpy()
+                frame = cv2.rectangle(
+                    frame, (int(x), int(y)), (int(w), int(h)), (36, 255, 12), 1
+                )
+                cv2.putText(
+                    frame,
+                    f"Label: {label}",
+                    (int(x), int(y) - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    (36, 255, 12),
+                    2,
+                )
             if current_word != len(words):
-                cv2.putText(frame, f'Score: {current_word}', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-                cv2.putText(frame, f'Current Word: {words[current_word]}', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-                cv2.putText(frame, f'Current Letter: {words[current_word][current_letter]}', (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                cv2.putText(
+                    frame,
+                    f"Score: {current_word}",
+                    (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    (36, 255, 12),
+                    2,
+                )
+                cv2.putText(
+                    frame,
+                    f"Current Word: {words[current_word]}",
+                    (10, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    (36, 255, 12),
+                    2,
+                )
+                cv2.putText(
+                    frame,
+                    f"Current Letter: {words[current_word][current_letter]}",
+                    (10, 80),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    (36, 255, 12),
+                    2,
+                )
             else:
-                cv2.putText(frame, f'Score: All Completed!', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+                cv2.putText(
+                    frame,
+                    "Score: All Completed!",
+                    (10, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    (36, 255, 12),
+                    2,
+                )
 
-            _, buffer = cv2.imencode('.jpg', frame)
+            _, buffer = cv2.imencode(".jpg", frame)
             frame = buffer.tobytes()
-            yield(b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
